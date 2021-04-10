59d524f80155082384ca6b9c78a94abbb412a397
arunsudhakar <arunsudhakar@gmail.com>
2021-04-10 23:57:23 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  airport_service:'http://localhost:8081',
+  star_service:'http://localhost:8082',
+  sid_service:'http://localhost:8083'
 };
