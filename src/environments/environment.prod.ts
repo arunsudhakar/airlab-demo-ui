d288cbdc20caec40d74a76a494e453a86b89f547
arunsudhakar <arunsudhakar@gmail.com>
2021-04-11 17:50:24 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  airport_service:'http://airport-service.airlab:8081',
-  sid_service:'http://sid-service.airlab:8082',
-  star_service:'http://star-service.airlab:8083',
+  airport_service:'http://54.224.21.95:8081',
+  sid_service:'http://54.224.21.95:8082',
+  star_service:'http://54.224.21.95:8083',
 };
